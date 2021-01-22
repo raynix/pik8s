0355e6b929ff8e68ac031692c2d95482e31f263c
Raymond Xu <raynix@gmail.com>
2021-01-22 11:28:18 +1100

    Fixed mysqldump file location.

--- dockers/backup/wordpress.sh
+++ dockers/backup/wordpress.sh
@@ -8,10 +8,12 @@ GCP_SA="$3"
 GCS="$4"
 
 backup_file="/tmp/wordpress-$BACKUP_NAME-$(date +%s).tar.gz"
-mysqldump -u $dbuser -h $dbhost -p$dbpass $dbname --opt > mysqldump.sql
-tar -czf $backup_file mysqldump.sql $ASSET_PATH
+mysqldump_file=/tmp/mysqldump.sql
+
+mysqldump -u $dbuser -h $dbhost -p$dbpass $dbname --opt > $mysqldump_file
+tar -czf $backup_file $mysqldump_file $ASSET_PATH
 
 gcloud auth activate-service-account --key-file=$GCP_SA
 gsutil cp $backup_file gs://$GCS
 
-rm $backup_file mysqldump.sql
+rm $backup_file $mysqldump_file
