e681bab39f461e28fac2d0c1ba769456e36c8ecc
Raymond Xu <raynix@gmail.com>
2021-01-28 11:22:25 +1100

    Added backup script for mastodon

---
+++ dockers/backup/mastodon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+BACKUP_NAME="$1"
+GCP_SA="$2"
+GCS="$3"
+
+backup_file="/tmp/mastodon-$BACKUP_NAME-$(date +%s).tar.gz"
+pgdump_file=/tmp/pgdump.sql
+
+PG_PASSWORD=$DB_PASS pg_dump -h $DB_HOST -u $DB_USER -p $DB_PORT $DB_NAME > $pgdump_file
+tar -czf $backup_file $pgdump_file
+
+gcloud auth activate-service-account --key-file=$GCP_SA
+gsutil -o "GSUtil:state_dir=/tmp/state_dir" cp $backup_file gs://$GCS
+
+rm $backup_file $pgdump_file
