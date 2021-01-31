d9972a945a203b09b515725b8c4b4c8332cea262
Raymond Xu <raynix@gmail.com>
2021-01-31 13:40:59 +1100

    Typo.

--- dockers/backup/mastodon.sh
+++ dockers/backup/mastodon.sh
@@ -9,7 +9,7 @@ GCS="$3"
 backup_file="/tmp/mastodon-$BACKUP_NAME-$(date +%s).tar.gz"
 pgdump_file=/tmp/pgdump.sql
 
-PG_PASSWORD=$DB_PASS pg_dump -h $DB_HOST -U $DB_USER -p $DB_PORT $DB_NAME > $pgdump_file
+PGPASSWORD=$DB_PASS pg_dump -h $DB_HOST -U $DB_USER -p $DB_PORT $DB_NAME > $pgdump_file
 tar -czf $backup_file $pgdump_file
 
 gcloud auth activate-service-account --key-file=$GCP_SA
