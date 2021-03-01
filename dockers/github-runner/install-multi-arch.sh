4f361fc9c5598bbc553c27e88b05f26f7ba6ab9e
Raymond Xu <raynix@gmail.com>
2021-03-02 08:37:15 +1100

    Added tk and kubectl for github-runner image

--- dockers/github-runner/install-multi-arch.sh
+++ dockers/github-runner/install-multi-arch.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 MACHINE=$(uname -m)
+
+# install github-runner
 case $MACHINE in
   x86_64) ARCH=x64 ;;
   armv7*) ARCH=arm ;;
@@ -13,3 +15,16 @@ curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v${
   tar xzf runner.tar.gz && \
   rm -rf runner.tar.gz
 
+# install tk
+case $MACHINE in
+  x86_64) ARCH=amd64 ;;
+  armv7*) ARCH=arm ;;
+  aarch64) ARCH=arm64 ;;
+esac
+
+curl -fSL -o /usr/local/bin/tk "https://github.com/grafana/tanka/releases/download/v${TK_VERSION}/tk-linux-${ARCH}"
+chmod a+x /usr/local/bin/tk
+
+# install kubectl
+curl -L -o /usr/local/bin/kubectl "https://dl.k8s.io/release/v${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl"
+chmod a+x /usr/local/bin/kubectl
