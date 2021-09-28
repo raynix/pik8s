51703768221abaae7f6eed471ea158f4f711338a
Raymond Xu <raynix@gmail.com>
2021-09-28 15:51:39 +1000

    Added sideloader image

---
+++ dockers/sideloader/sideloader.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+DEST=${DEST:-/sideloader}
+EXECUTABLE=${EXECUTABLE:-no}
+set -xe
+
+until [ -z ${1+x} ] || [ -z ${2+x} ]; do
+    curl -fSL --create-dirs -o $DEST/$1 $2
+    shift 2
+done
+if [ "$EXECUTABLE" = "yes" ]; then
+    chmod +x $DEST/*
+fi
+
+ls -lht $DEST
+
